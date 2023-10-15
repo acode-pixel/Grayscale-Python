e634143f623d9bf5dbfe8b6cd7a7ecb26f3cb1d7
acode.pixel <donaldanthony245@gmail.com>
2023-10-15 18:45:00 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import sys, time
+from tqdm import trange
 
 def main():
     start = time.time()
@@ -12,40 +13,44 @@ def main():
     neighboringPixelDiff = float(sys.argv[4])
     skipped = 0
     rawData = b''
+    area = im.size[0]*im.size[1]
 
-    while True:
+    with trange(area, desc="Pixels Processed") as pBar:
+        
+        while True:
+            prevShadeColor = 0
+            prevPixelColorAverg = 0
 
-        prevShadeColor = 0
-        prevPixelColorAverg = 0
+            while pixel_xy[0] < im.size[0]:
 
-        while pixel_xy[0] < im.size[0]:
+                scale = 0
+                pixel_color = im.getpixel(tuple(pixel_xy))
+                pixelColorAverg = (pixel_color[0]+pixel_color[1]+pixel_color[2])/3
 
-            scale = 0
-            pixel_color = im.getpixel(tuple(pixel_xy))
-            pixelColorAverg = (pixel_color[0]+pixel_color[1]+pixel_color[2])/3
-
-            if abs((pixelColorAverg/255)-(prevPixelColorAverg/255)) <= neighboringPixelDiff:
-                skipped += 1
-                rawData += int(prevShadeColor).to_bytes(1, "little")
-                pixel_xy[0] += 1
-                continue
+                if abs((pixelColorAverg/255)-(prevPixelColorAverg/255)) <= neighboringPixelDiff:
+                    skipped += 1
+                    rawData += int(prevShadeColor).to_bytes(1, "little")
+                    pBar.update(1)
+                    pixel_xy[0] += 1
+                    continue
             
-            while True:
-                print(scale*grayscaleDiff)
-                if 255-(scale*grayscaleDiff) <= pixelColorAverg:
-                    prevShadeColor = 255-(scale*grayscaleDiff)
-                    prevPixelColorAverg = pixelColorAverg
-                    rawData += int(255-(scale*grayscaleDiff)).to_bytes(1, "little")
-                    break
-                scale += 1
+                while True:
+                   #print(scale*grayscaleDiff)
+                   if 255-(scale*grayscaleDiff) <= pixelColorAverg:
+                       prevShadeColor = 255-(scale*grayscaleDiff)
+                       prevPixelColorAverg = pixelColorAverg
+                       rawData += int(255-(scale*grayscaleDiff)).to_bytes(1, "little")
+                       pBar.update(1)
+                       break
+                   scale += 1
 
-            pixel_xy[0] += 1
+                pixel_xy[0] += 1
 
-        pixel_xy[0] = 0
-        pixel_xy[1] += 1
+            pixel_xy[0] = 0
+            pixel_xy[1] += 1
 
-        if pixel_xy[1] >= im.size[1]:
-            break
+            if pixel_xy[1] >= im.size[1]:
+                break
     
     print("Completed in " + str(round(time.time()-start, 2)) + "s")
     print("Skipped " + str(skipped))
